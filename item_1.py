b9b41f039d473950040aca7b83e2550c58e0fd90
borg <elm.tiago@gmail.com>
2020-03-12 03:28:38 -0300

    resultado satisfatório

--- item_1.py
+++ item_1.py
@@ -11,99 +11,142 @@ import math
 # If you want to open a video, just change v2.VideoCapture(0) from 0 to the filename, just like below
 cap = cv2.VideoCapture('VID_20200302_063445951.mp4')
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+HEIGHT = cap.read()[1].shape[0]
+WIDTH = cap.read()[1].shape[1]
+print("Video width:",WIDTH)
+print("Video height:",HEIGHT)
+
 pi = np.pi
+
 lower = 0
 upper = 1
 font = cv2.FONT_HERSHEY_SIMPLEX
 font2 = cv2.FONT_HERSHEY_PLAIN
 
 print("Press q to QUIT")
-
+colors = {
+    "vermelho":(0,0,255),
+    "azul":(255,0,0)
+}
 def auto_canny(image, sigma=0.33):
     """apply automatic Canny edge detection using the computed median"""
     v = np.median(image)
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
     edged = cv2.Canny(image, lower, upper)
-
     return edged
+
+def coeficientes( array ):
+    """Devolve uma tupla (m,n). m é o coeficiente angular e n é o coeficiente linear 
+    da reta que representa o conjunto de retas do array"""
+    if len(array) > 0:
+        # array[:,t] -> coluna t da matriz chamada array
+        x1, y1, x2, y2 = array[:,0], array[:,1], array[:,2], array[:,3]
+        # m1 = (y2 - y1)/(x2-x1)
+        m = (y2 - y1)/(x2 - x1)
+        # n1 = m1*x1 + y1
+        n = -m*x1 + y1
+        
+        m = round(np.median(m),2)
+        n = round(np.median(n),2)
+        return m,n
+def tangente(linha):
+    "Devolve a tangente do ângulo formado entre a linha e a horizontal (no referencial da imagem)"
+    x1,y1,x2,y2 = linha[0]
+    return (y2-y1)/(x2-x1)
+
+def draw_line(img, linha, color:str):
+    color = colors[color]
+    x1,y1,x2,y2 = linha[0]
+    cv2.line(img, (x1, y1), (x2, y2), color, 2) 
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     
-    frame_hsv = cv2.cvtColor (frame, cv2.COLOR_BGR2HSV)
-    
-    # Convert the frame to grayscale
+    # Conversão para escala de cinza
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #hsv's do branco
-    hsv1, hsv2 = aux.ranges('#ffffff')
-    
-    #Mask do branco
-    mask = cv2.inRange(frame, hsv1, hsv2)
-    
-    ##Seleção
-    edges = cv2.Canny(gray, 50, 200)
-    
-    # Detect the edges present in the image
+    # Redução de ruído 1
+    gray = cv2.GaussianBlur(gray,(5,5), 0.33)
 
-    # A gaussian blur to get rid of the noise in the image
-    # blur = cv2.GaussianBlur(gray,(5,5),0)
-    
-    # bordas = auto_canny(blur)
-    
-    lines = cv2.HoughLinesP(edges, 1, pi/180, 100, minLineLength=80, maxLineGap=5)
-    print(lines.shape)
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        try:
-            tangente = (y2-y1)/(x2-x1) # inclinação
-            if abs(tangente) > 0.6 and abs(tangente) < 0.9:
-                cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 3)
-        except:
-            pass
+    # limiarização da imagem ( fonte: https://youtu.be/P2R7Nn1_VwQ )
+    limiar,img_limiar = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)
 
+    # Redução de ruido 2
+    # Aplicação de um kernel personalizado (deve existir forma de remover a repetição)
+    KERNEL = np.array([
+        [0,0,1,0,0],
+        [0,0,1,0,0],
+        [0,1,1,1,0],
+        [0,1,1,1,0],
+        [0,1,1,1,0],
+        [0,0,1,0,0],
+        [0,0,1,0,0]
+    ], dtype="uint8")
+    
+    segmentado = cv2.morphologyEx(img_limiar,cv2.MORPH_OPEN, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_OPEN, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_OPEN, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_OPEN, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_CLOSE, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_CLOSE, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_CLOSE, KERNEL)
+    segmentado = cv2.morphologyEx(segmentado,cv2.MORPH_CLOSE, KERNEL)
+
+    # Redução de ruído 3
+    blur = cv2.GaussianBlur(segmentado,(11,11),0.33)
+
+    # Detecção de contornos
+    shapes = cv2.Canny(blur, 50, 200)
+
+    # A partir dos contornos, gera um array com linhas que satisfazem os argumentos 
+    # minLineLength e maxLineGap (parâmetros foram modificados até que um resultado
+    # satisfatório fosse atingido)
+    lines = cv2.HoughLinesP(shapes, 1, pi/180, 50, minLineLength=100, maxLineGap=20)
+    
     OUTPUT = frame
-
-    # Draw Lines
-    lines = None
-    lines = cv2.HoughLines(bordas, 20, pi/180, 10, None, 0, 0)  
     
-    if lines is not None:      
-        lines = np.uint16(np.around(lines))
-        print(lines.shape)
-
-        for i in range(0, len(lines)):
-
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-
-            a,b = math.cos(theta), math.sin(theta)
-            x0,y0 = a*rho, b*rho
-
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-
-            # draw the outer line
-            cv2.line(OUTPUT, pt1, pt2, (0,255,0), 2, cv2.LINE_AA)
-
-    # Linha default
-    # cv2.putText(img, text, org, fontFace, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
-    try: 
-        # cv2.line(frame, (lines[0], lines[1]), (0,255,0), 2)
-        cv2.line(blur, (lines[0], lines[1]), (0,255,0), 2)        
+    red_lines = []
+    blue_lines = []
+    
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            try:
+                # filtrar as retas por inclinação
+                if 0.7 < tangente(line) < 1.3: 
+                    red_lines.append(line[0])
+                    draw_line(OUTPUT, line, "vermelho") # desenha linhas vermelhas
+                elif -1.3 < tangente(line) < -0.7: 
+                    blue_lines.append(line[0])
+                    draw_line(OUTPUT, line, "azul") # desenha linhas azuis
+            except:
+                pass
+    red_lines = np.array(red_lines)
+    blue_lines = np.array(blue_lines)
+    try:
+        # função 'coeficientes' definida lá em cima
+        # m e n são conficientes de retas da forma:  y = mx + n
+        m1,n1 = coeficientes( red_lines  )
+        m2,n2 = coeficientes( blue_lines )
     except:
         pass
+    finally:
+        # min() e max() servem para deixar o circulo sempre visível
+        # X e Y são as coordenadas do ponto de encontro entre a reta média azul e a reta média vermelha
+        X = max(0, min( int((n2-n1)/(m1-m2)), WIDTH ) ) 
+        Y = max(0, min( int((m1*X + n1)), HEIGHT ) ) 
+        # print("(X,Y) =",(X,Y)) # descomente essa linha para printar no terminal 
+        # as coordenadas do centro
+        cv2.circle(OUTPUT, (X,Y), 10, (0,255,0), 2, 2)
     
-    # adicionar textos na tela:
+    
+    # Adicionar textos na tela
     cv2.putText(OUTPUT," Aperte q para sair", (0,50), font, 1,(255,255,255),2,cv2.LINE_AA)
-    #More drawing functions @ http://docs.opencv.org/2.4/modules/core/doc/drawing_functions.html
 
-    # Display the resulting frame
-    cv2.imshow("outpu", OUTPUT)
+    # Exibir o resultado
+    cv2.imshow("output", OUTPUT)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
